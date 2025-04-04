b1049842b231a9ba4ac82a24e59d43e2521a852d
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-04-04 20:14:05 +0500

    localAIAgent realistic restaurant review

--- vector.py
+++ vector.py
@@ -3,3 +3,9 @@ from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import os
 import pandas as pd
+
+df = pd.read_csv("realistic_restaurant_reviews.csv")
+embeddings = OllamaEmbeddings(model="mxbai-embed-large")
+
+db_location = "./chrome_langchain_db"
+add_documents = not os.path.exists(db_location)
