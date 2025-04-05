aa8bf13e68069603dac7840f35f4ec4208618b1b
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-04-05 16:30:06 +0500

    localAIAgent realistic restaurant review

--- vector.py
+++ vector.py
@@ -25,5 +25,13 @@ if add_documents:
 
 vector_store = Chroma(
     collection_name ="restaurant_reviews",
-    persist_directory=db_location
+    persist_directory=db_location,
+    embedding_function=embeddings
+)
+
+if add_documents:
+    vector_store.add_documents(documents=documents,ids=ids)
+
+retriever = vector_store.as_retriever(
+    search_kwargs={"k":5}
 )
